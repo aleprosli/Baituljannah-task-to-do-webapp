7d0b62d977503bd8a9b3cdfcee5351cfe6b6d4a3
Aleprosli <aliff.rosli96@gmail.com>
2021-11-01 18:45:50 +0800

    add hasmany todolists relation in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function todolists()
+    {
+        return $this->hasMany(Todolist::class);
+    }
 }
